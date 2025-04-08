2723fab00646841291c13d46642677aee9853daa
Kirill <kreshetov12@gmail.com>
2025-04-08 11:44:53 +0200

    modificando archivos para despliegue

--- web-react-app/src/App.tsx
+++ web-react-app/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'; // Cambiar a HashRouter
 import NavBar from './pages/NavBar';
 import Gatos from './pages/Gatos';
 import AboutUs from './pages/AboutUs';
