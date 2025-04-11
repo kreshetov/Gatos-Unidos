79ac22a73172c3dde5535cfcb91f082811aee8f1
Kirill <kreshetov12@gmail.com>
2025-04-11 14:04:31 +0200

    agregado footer y actualizado css

---
+++ web-react-app/src/pages/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import '../style.css';
+
+const Footer = () => {
+    return (
+        <footer>
+            <p> Gatos Unidos. Proyecto 2025 Kirill Reshetov Dmitriev </p>
+        </footer>
+    )
+};
+
+export default Footer;
