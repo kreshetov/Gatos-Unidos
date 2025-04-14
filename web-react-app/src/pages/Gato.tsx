ebc59d468e539846bb90f8f41f895223d6199138
Kirill <kreshetov12@gmail.com>
2025-04-14 22:10:23 +0200

    creado componente gato

---
+++ web-react-app/src/pages/Gato.tsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+interface ingerfazGato {
+    id : number;
+    nombre: string;
+    raza: string;
+    edad: string;
+    sexo: string;
+    foto: string;
+    descripcion: string;
+}
+
+const Gato = () => {
+    const { id } = useParams<{ id: string }>();
+    const [gato, setGato] = useState<ingerfazGato | null>(null);
+
+    useEffect(() => {
+        fetch(`https://d4bc0a96-96f3-438e-9801-5a3937946062.mock.pstmn.io/Gatos/${id}`)
+            .then((response) => response.json())
+            .then((data) => setGato(data))
+            .catch((error) => console.error('Error al obtener el gato', error));
+    }, [id]);
+
+    if (!gato) {
+        return <p>Cargando gato...</p>;
+    }
+
+    return (
+        <div>
+            <div className="informacionGato">
+                <div className="gato" key={gato.id}>
+                    <img src={gato.foto} alt={gato.nombre} />
+                    <p>Conoce a {gato.nombre}</p>
+                    <p>{gato.raza}</p>
+                    <p>{gato.edad}</p>
+                    <p>{gato.sexo}</p>
+                    <p>{gato.descripcion}</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Gato;
